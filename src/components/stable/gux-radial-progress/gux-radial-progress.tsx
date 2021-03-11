88639e31d4325df7b1435fe782d584ee085690d2
Daragh King <daragh.king@genesys.com>
2021-03-11 18:32:09 +0000

    feat(gux-radial-progress): added accent property
    added accent property
    
    COMUI-422

--- src/components/stable/gux-radial-progress/gux-radial-progress.tsx
+++ src/components/stable/gux-radial-progress/gux-radial-progress.tsx
@@ -2,6 +2,8 @@ import { Component, Element, h, JSX, Prop } from '@stencil/core';
 
 import { trackComponent } from '../../../usage-tracking';
 
+import { GuxRadialProgressAccent } from './gux-radial-progress.types';
+
 const RADIUS = 23.5;
 const STROKE_DASH = 2 * Math.PI * RADIUS;
 
@@ -27,25 +29,36 @@ export class GuxRadialProgress {
   @Prop()
   max: number = 100;
 
+  /**
+   * Set the color of the progress element
+   */
+  @Prop()
+  accent: GuxRadialProgressAccent = 'neutral';
+
   private dropshadowId = `dropshadow-${idCounter++}`;
 
   componentWillLoad(): void {
-    trackComponent(this.root);
+    trackComponent(this.root, { variant: this.accent });
   }
 
   render(): JSX.Element {
     return this.canShowPercentage(this.value, this.max)
-      ? this.showPercentageState(this.value, this.max)
+      ? this.showPercentageState(this.value, this.max, this.accent)
       : this.showSpinnerState();
   }
 
-  private canShowPercentage(value, max) {
+  private canShowPercentage(value: number, max: number): boolean {
     return !(isNaN(value) || isNaN(max) || value > max || value < 0);
   }
 
-  private showPercentageState(value, max): JSX.Element {
+  private showPercentageState(
+    value: number,
+    max: number,
+    accent: GuxRadialProgressAccent
+  ): JSX.Element {
     return (
       <div
+        class={`gux-${accent}`}
         role="progressbar"
         aria-valuenow={value}
         aria-valuemin="0"
---
+++ src/components/stable/gux-radial-progress/gux-radial-progress.types.ts
@@ -0,0 +1,5 @@
+export type GuxRadialProgressAccent =
+  | 'neutral'
+  | 'positive'
+  | 'alert'
+  | 'warning';
